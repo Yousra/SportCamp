1f40f48422afc08d8b590168d8341cd228a8e646
Yousra <yskamoona@spartans.manchester.edu>
2011-05-12 08:24:59 -0400

    Third commit

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host="localhost";
-$dbh=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("phplogin") or die('I cannot select the database because: ' . mysql_error());
-?>
